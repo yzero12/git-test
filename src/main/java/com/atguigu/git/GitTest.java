7b7dc6fed3ecdd45b24a224da09aee15fa2a17f1
yzero <yzero@atguigu.com>
2022-08-19 14:07:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author zjzstart
+ * @create 2022-08-19 13:59
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
